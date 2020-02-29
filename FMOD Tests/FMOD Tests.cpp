831a57823d7bee54dc9945ff9c0a824590ee45a8
M. O. Marmalade <xephyrpanda@yahoo.com>
2020-02-28 19:40:39 -0800

    Learning about sustain point behavior and timestretching behavior

--- FMOD Tests/FMOD Tests.cpp
+++ FMOD Tests/FMOD Tests.cpp
@@ -1,8 +1,7 @@
-
-
 #include <Windows.h>
 #include <iostream>
 #include <string>
+#include <vector>
 #include <thread>
 #include <chrono>
 #include <cstdlib>
@@ -15,7 +14,106 @@ using namespace std;
 int main()
 {
 
+	CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
+
 	FMOD_RESULT result;
+	FMOD::Studio::System* system = NULL;
+
+	result = FMOD::Studio::System::create(&system);
+
+	result = system->initialize(200, FMOD_STUDIO_INIT_NORMAL, FMOD_INIT_NORMAL, 0);
+
+	FMOD::Studio::Bank* masterBank = NULL;
+	result = system->loadBankFile("media/Master.bank", FMOD_STUDIO_LOAD_BANK_NORMAL, &masterBank);
+
+	FMOD::Studio::Bank* stringsBank = NULL;
+	result = system->loadBankFile("media/Master.strings.bank", FMOD_STUDIO_LOAD_BANK_NORMAL, &stringsBank);
+
+	FMOD::Studio::Bank* bankA = NULL;
+	result = system->loadBankFile("media/Bank A.bank", FMOD_STUDIO_LOAD_BANK_NORMAL, &bankA);
+
+	
+	int bankAEventCount;
+	result = bankA->getEventCount(&bankAEventCount);
+	cout << bankAEventCount << " events found!\n";
+
+
+	int bankAEventsLoadedCount;
+	vector<FMOD::Studio::EventDescription*> bankAEventList;
+	bankAEventList.resize(bankAEventCount);	
+	result = bankA->getEventList(bankAEventList.data(), bankAEventCount, &bankAEventsLoadedCount);
+	cout << bankAEventsLoadedCount << " event loaded!\n";
+
+
+	vector<FMOD::Studio::EventInstance*> bankAEvents;
+	bankAEvents.resize(bankAEventsLoadedCount);
+	for (int x = 0; x < bankAEventsLoadedCount; x++) {
+		result = bankAEventList[x]->createInstance(&bankAEvents[x]);
+	}	
+
+	
+	bankAEvents[0]->start();
+	bankAEvents[0]->triggerCue();
+	result = system->update();
+
+	cout << "Press G Key to lengthen timestretch\npress H Key to shorten timestretch\npress X Key to quit\n";
+
+	bool zKey = false;
+	bool xKey = false;
+	bool gKey = false;
+	bool hKey = false;
+	bool holdZKey = false;
+	bool holdGKey = false;
+	bool holdHKey = false;
+	bool counter = false;
+	float timeStretchValue = 1.0f;
+	while (!counter) {
+
+		zKey = ((0x8000 & GetAsyncKeyState((unsigned char)("Z"[0]))) != 0);
+		xKey = ((0x8000 & GetAsyncKeyState((unsigned char)("X"[0]))) != 0);
+		gKey = ((0x8000 & GetAsyncKeyState((unsigned char)("G"[0]))) != 0);
+		hKey = ((0x8000 & GetAsyncKeyState((unsigned char)("H"[0]))) != 0);
+
+		if (zKey && !holdZKey) {		
+			bankAEvents[0]->triggerCue();			
+			holdZKey = true;
+		}
+		else if (!zKey && holdZKey) {
+			holdZKey = false;
+		}
+
+		if (gKey && !holdGKey) {
+			timeStretchValue -= 0.05f;
+			bankAEvents[0]->setParameterByName("TimeStretch", timeStretchValue);
+			holdGKey = true;
+			cout << "Current timestretch: " << timeStretchValue << endl;
+		}
+		else if (!gKey && holdGKey) {
+			holdGKey = false;
+		}
+
+		if (hKey && !holdHKey) {
+			timeStretchValue += 0.05f;
+			bankAEvents[0]->setParameterByName("TimeStretch", timeStretchValue);
+			holdHKey = true;
+			cout << "Current timestretch: " << timeStretchValue << endl;
+		}
+		else if (!hKey && holdHKey) {
+			holdHKey = false;
+		}
+
+		if (xKey) {
+			counter = true;
+		}		
+
+		result = system->update();
+	}
+	
+	result = system->release();
+	CoUninitialize();
+
+	return 0;
+	/*FMOD_RESULT result;
 	FMOD::System* system = NULL;
 
 	result = FMOD::System_Create(&system);      // Create the main system object.
@@ -55,7 +153,7 @@ int main()
 	result = rdts->release();
 	result = system->close();
 	result = system->release();
-
+	*/
 	
 	/*FMOD_RESULT result;
 	FMOD::Studio::System* system = NULL;
